d545079345dd998b8e749d413dd09ddbaf97ace5
rok <roky.zz@gmail.com>
2021-07-24 19:41:47 +0200

    I really like this

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+			Let's not learn React because we already know it!
         </a>
       </header>
     </div>
